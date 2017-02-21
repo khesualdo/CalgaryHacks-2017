3d6a61ca16d6b5e900219591649d6033800f5cc7
00111000 <medvegalapa@gmail.com>
2017-02-21 16:46:22 -0700

    Edits inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,21 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Hello. This message was sent from scripts/inject.js");
+			// ----------------------------------------------------------
+
+			chrome.tabs.getCurrent(function(tab) {
+			    // var url = tabs[0].url;
+					console.log(tab);
+			});
+
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray){
+				console.log(tabArray);
+			});
 
 	}
 	}, 10);
