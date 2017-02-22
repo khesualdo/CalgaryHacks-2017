c02bd438e3fdd2f8061a567d98588abcfd3314ee
khesualdobarykin@gmail.com <medvegalapa@gmail.com>
2017-02-22 12:49:05 -0700

    Added styles for inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -22,6 +22,10 @@ function removeBodyHTML() {
 
 	// Set the body background color
 	//document.body.style.backgroundColor = "black";
+	document.body.style.zIndex = "3000 !important";
+	document.body.style.boxShadow = "inset 0 0 10em darkgreen !important";
+	document.body.style.background = "linear-gradient( to bottom, #5d9634, #5d9634 50%, #538c2b 50%, #538c2b ) !important";
+	document.body.style.backgroundSize = "100% 20px !important";
 
 	// Append the timer to the body tag
 	bodyTag.appendChild(timer);
