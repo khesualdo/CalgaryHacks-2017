b6d782fc44468da5f6e68dad4bc9e23979139ea8
Seavanas <sw92410@hotmail.com>
2017-02-21 19:50:49 -0700

    thangs

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,25 +1,40 @@
-$(function () 
-{
-	$('#datetimepicker4').datetimepicker();
-});
+	$('#datetimepicker4').datetimepicker({});
 
-$(function () 
-{
 	$("#duration").durationPicker(
 	{
 		hours: 
 		{
 			label: "h",
 		    min: 0,
-		    max: 24
+		    max: 999
 	  	},
 	  	minutes: 
 	  	{
 		    label: "m",
 	    	min: 0,
-	    	max: 24
+	    	max: 59
 	  	},
 	  	classname: 'form-control',
 	  	responsive: true
 	});
+
+var blockList = document.getElementById("blockList");
+console.log(document.getElementById("url"));
+var url = document.getElementById("url").value;
+//var date = document.getElementById("date").value;
+var duration = document.getElementById("duration");
+
+
+function addToBlockList()
+{
+	var newBlockItem = document.createElement("li");
+	newBlockItem.appendChild(document.createTextNode(url));
+	blockList.appendChild(newBlockItem);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var temp = document.getElementById('addBlockButton');
+    temp.addEventListener('click', function() {
+        addToBlockList();
+    });
 });
