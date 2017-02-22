c7d5ecc1294c76b660800e10ee9a7939aececa63
Stepan Fedorko-Bartos <Step7750@gmail.com>
2017-02-21 18:18:39 -0700

    Adds BG Support

--- src/bg/background.js
+++ src/bg/background.js
@@ -9,5 +9,5 @@
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+
+function updateInterval() {
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,25 +1,26 @@
-
-
 var bodyTag = document.getElementsByTagName('body')[0];
 
 // Removes HTML inside the body tag and display the timer
-function init(){
+function updateTimer(duration){
 
-	//removeBodyHTML();
-	displayTimer();
-}
-
-// Removes HTML inside the body tag
-function removeBodyHTML(){
+	// clear body every time
 	bodyTag.innerHTML = " ";
+
+	// Displays the timer
+	// If the amountOfSeconds is -1, then block infinitly
+	// Else block for some amount of time
+
+	// CREATE TIMER ELEMENTS IN DOM
+	console.log(duration);
 }
 
-// Displays the timer
-// If the amountOfSeconds is -1, then block infinitly
-// Else block for some amount of time
-function displayTimer(amountOfSeconds){
 
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  	if (request.type == "updateInterval") {
+  		updateTimer(request.duration);
+  	}
+});
 
-}
-
-init();
+chrome.extension.sendMessage({type: "newTab"}, function (response) {
+	console.log(response);
+});
