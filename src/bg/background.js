f8ac4d8bc6c09b734e8a7973f54ee28648daa0cc
Khesualdo Condori <medvegalapa@gmail.com>
2017-08-07 23:12:03 -0600

    Added comments

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,6 @@
 // Bind event listener
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+
     if (request.type == "addTimeout") {
         addTimeout(request);
         sendResponse();
@@ -20,33 +21,45 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
         });
     }
 
-    return true; // return sendResponse async
+    // Indicates that sendResponse() will be sent asynchronously
+    return true;
 });
 
-// Adds a timeout
+// Add a timeout to Chrome storage 
 function addTimeout(timeout) {
+
+    // Request `timeouts` object from Chrome storage 
     chrome.storage.sync.get("timeouts", function (timeouts) {
-        if (!timeouts.timeouts) timeouts = {timeouts: []};
+
+        // If no `timeouts` exits, create a `timeouts` object
+        if (!timeouts.timeouts)
+            timeouts = {timeouts: []};
 
         var timeoutArray = timeouts.timeouts;
 
         delete timeout.type;
 
+        // Add new timeout to `timeoutArray`
         timeoutArray.push(timeout);
 
         chrome.storage.sync.set({'timeouts': timeoutArray}, () => {});
     });
 }
 
-// Remove a timeout (clears it for the next interval)
+// Remove a timeout from Chrome storage
+// Clears timeout for the next interval
 function removeTimeout(timeout) {
     chrome.storage.sync.get("timeouts", function (timeouts) {
-        if (!timeouts.timeouts) timeouts = {timeouts: []};
+
+        // If no `timeouts` exits, create a `timeouts` object
+        if (!timeouts.timeouts)
+            timeouts = {timeouts: []};
 
         var timeoutArray = timeouts.timeouts;
 
         delete timeout.type;
 
+        // Find timeout in the timeoutArray
         let index = timeoutArray.findIndex((e) => {
             if (e.startAt == timeout.startAt && e.duration == timeout.duration && e.filters == timeout.filters) {
                 return true;
@@ -54,38 +67,50 @@ function removeTimeout(timeout) {
             else return false;
         });
 
+        // If not timeout was found, then return
         if (index == -1) return;
 
+        // Set the `startAt` and `duration` of the timeout to 0
         timeoutArray[index].startAt = 0;
         timeoutArray[index].duration = 0;
 
+        // Push the updated `timeouts` object to Chrome storage
         chrome.storage.sync.set({'timeouts': timeoutArray}, () => {});
     });
 }
 
-// Loops and updates all tabs
+// Updates all tabs, which have timeouts
 function updateInterval() {
     chrome.tabs.query({}, function(tabs) {
         chrome.storage.sync.get('timeouts', function (timeouts) {
-            if (!timeouts.timeouts) timeouts = {timeouts: []};
+
+            // If no `timeouts` exits, create a `timeouts` object
+            if (!timeouts.timeouts)
+                timeouts = {timeouts: []};
 
             let updatedTimeouts = [];
 
             for (let timeout of timeouts.timeouts) {
-                // Get the tabs that match this timeout
+
+                // Get the tabs that matches this timeout
                 let matchedTabs = getTabsThatMatch(tabs, timeout.filters);
 
+                // Calculate the end time of timeout
                 let timeoutEnd = timeout.startAt + timeout.duration;
+
+                // Get current time
                 let currentTime = Math.floor(Date.now() / 1000);
 
                 if (currentTime > timeoutEnd) {
-                    // tell any applicable tabs the timeout ended
+
+                    // Tell any applicable tabs the timeout ended
                     for (let tab of matchedTabs) {
                         chrome.tabs.sendMessage(tab.id, {type: "endInterval"});
                     }
                 }
                 else if (currentTime >= timeout.startAt) {
-                    // update each tab
+                    
+                    // Update each tab
                     for (let tab of matchedTabs) {
                         chrome.tabs.sendMessage(tab.id, {type: 'updateInterval', duration: timeoutEnd - currentTime});
                     }
@@ -97,9 +122,8 @@ function updateInterval() {
                 }
             }
 
-            // update the filters in the db
-
-            // Only update if something changed to get under write quotas
+            // Update `timeouts` object in Chrome storage 
+            // only if something changed, to get under write quotas
             if (updatedTimeouts.length != timeouts.timeouts.length) {
                 chrome.storage.sync.set({'timeouts': updatedTimeouts}, () => {});
             }
@@ -109,14 +133,19 @@ function updateInterval() {
 
 // Returns tabs that match the url array
 function getTabsThatMatch(tabs, urls) {
+
     var returnTabs = [];
 
+    // Traverse through currently opened tabs
     for (let tab of tabs) {
-        let domain = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/.exec(tab.url);
-        domain = domain[1]; // remove www
 
+        let domain = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/.exec(tab.url);
+
+        // Remove `www` from the domain
+        domain = domain[1];
+
+        // We found a tab, append it to the `returnTabs` array
         if (urls.indexOf(domain) > -1) {
-            // found it
             returnTabs.push(tab);
         }
     }
@@ -124,7 +153,7 @@ function getTabsThatMatch(tabs, urls) {
     return returnTabs;
 }
 
-
+// Update all timeouts every 0.5 seconds
 setInterval(() => {
     updateInterval();
 }, 500);
--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,7 +1,10 @@
 $('#datepick').datetimepicker();
 $("#duration").durationPicker();
 
+// Formats the seconds into hours, minutes, and seconds
+// Example: 3780 seconds --> 1h 3m
 function formatSeconds(sec) {
+
 	let hrs = Math.floor(sec / 3600);
     let min = Math.floor(sec % 3600 / 60);
 
@@ -13,18 +16,28 @@ function formatSeconds(sec) {
     return display;
 }
 
+// Creates a new timeout and sends a message to `background.js`
 function createTimeout() {
+
+	// Get the element containing all current timeouts
 	let timeoutList = document.getElementById("timeoutList");
+
+	// Get information entered by the user
 	let url = document.getElementById("url").value.replace("www.", "").replace("https://", "").replace("http://", "").split("/")[0];
 	let date = document.getElementById("datepick").value;
 	let duration = parseInt(document.getElementById("duration").value);
-	
+
+	// Create a new timeout, if user entered valid information
 	if (url && date && duration > 0) {
+
+		// Create a new div with necessary information
 		let newTimeout = document.createElement("div");
 		newTimeout.appendChild(document.createTextNode(url + " - " + date + " - " + formatSeconds(duration)));
 
+		// Add a new div to the list of other timeouts
 		timeoutList.appendChild(newTimeout);
 
+		// Create a call object
 		let obj = {
 			startAt: Date.parse(date)/1000,
 			duration: duration,
@@ -32,25 +45,32 @@ function createTimeout() {
 			type: "addTimeout"
 		};
 
+		// Send a message to `background.js`
 		chrome.extension.sendMessage(obj, () => {});
 	}
 }
 
+// Gets the URL of the current page and pastes it into the `Website Filter:` input field
 function getCurrentUrl() {
 	chrome.extension.sendMessage({type: "getActiveTab"}, (tab) => {
 		if (!tab) return;
-		
 		document.getElementById("url").value = tab.url.replace("http://", "").replace("https://", "");
 	});
 }
 
+// Fetched timeouts from Chrome storage and displays them
 function populateTimeouts() {
 	chrome.extension.sendMessage({type: "getTimeouts"}, (timeouts) => {
+
+		// Timeouts from Chrome storage
 		timeouts = timeouts.timeouts;
 
+		// Get the element containing all current timeouts
 		let timeoutList = document.getElementById("timeoutList");
 
+		// Iterate over timeouts and append them to `timeoutList`
 		for (let timeout of timeouts) {
+
 			let newTimeout = document.createElement("div");
 			let date = moment.unix(timeout.startAt).format('MM/DD/YYYY h:mm A');
 
@@ -61,6 +81,7 @@ function populateTimeouts() {
 	});
 }
 
+// Displays all current timeouts
 populateTimeouts();
 
 $("#createTimeout").click(() => {
